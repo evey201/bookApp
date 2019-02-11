0186d5d3ebc1b42c87690af324f9ec31ea1718c8
Evey Alabi <evesho.alabi@gmail.com>
2019-02-11 11:13:35 +0100

    login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,12 +75,13 @@ exports.login = async (req, res, next) => {
             const token = jwt.sign(
                 {
                 email: loadedUser.email, 
+                name: loadedUser.name,
                 userId: loadedUser._id.toString()
                 }, 
                     'secret', 
                     { expiresIn: '1h'}
             );
-            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+            res.status(200).json({ token: token, userId: loadedUser._id.toString(), name: loadedUser.name });
         })
         .catch(err => {
             if (!err.status) {
