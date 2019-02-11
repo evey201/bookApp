bd4c2dbc89e4b8e099b022a7e08bd47eeb88f082
Evey Alabi <evesho.alabi@gmail.com>
2019-02-11 11:12:18 +0100

    login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,30 @@
 const User = require('../server/models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { body } = require('express-validator/check');
 const { validationResult } = require('express-validator/check');
 
 //SignUp controller
 exports.signup = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ error: errors.array() });
+        }
     
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
+        await User.create({
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, 10),
+            name:req.body.name
+        })
+        
+        return res.status(201).json({success: true, message: 'User Succesfully created'})
+        
+    } catch (error) {
+        return res.status(500).json({ error });
 
-    const user = await User.create({
-        email: req.body.email,
-        password: await bcrypt.hash(req.body.password, 10),
-        name:req.body.name
-    })
+    }
     
-    return res.status(201   ).json({success: true, message: 'User Succesfully created'})
 
 
 };
@@ -45,3 +52,41 @@ exports.signUpValidation = [
          .not()
         .isEmpty()
 ] 
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({email: email})
+        .then(user => {
+            if (!user) {
+                const Error = new Error('A user with this email could not be found');
+                err.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        }).then(isEqual => {
+            if (!isEqual) {
+                const Error = new Error('Password is does not match the User');
+                err.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign(
+                {
+                email: loadedUser.email, 
+                userId: loadedUser._id.toString()
+                }, 
+                    'secret', 
+                    { expiresIn: '1h'}
+            );
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+        })
+        .catch(err => {
+            if (!err.status) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { body } = require('express-validator/check');
 const router = express.Router();
-const {signup, signUpValidation} = require('../controllers/authController');
+const {signup, signUpValidation, login} = require('../controllers/authController');
 
 const User = require('../server/models/user');
 
 router.put('/signup', signUpValidation, 
     signup
 );
+
+router.post('/login', login )
 
 module.exports = router;
